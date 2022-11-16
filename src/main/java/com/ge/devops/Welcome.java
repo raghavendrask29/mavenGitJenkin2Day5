1a7fa9cc4bd4db9e510d4da62ab87241acd24d62
raghavendrask29 <raghavendra.sk29@gmail.com>
2022-11-16 11:11:35 +0530

    day3 commit1

---
+++ src/main/java/com/ge/devops/Welcome.java
@@ -0,0 +1,11 @@
+package com.ge.devops;
+
+public class Welcome {
+
+        public static void main( String[] args )
+        {
+            System.out.println( "Welcome World Test!" );
+        }
+    
+
+}
